e0376d21590ca993843dd9f21745d7e174287cac
Oren <ogolan@attinteractive.com>
2012-07-10 05:44:21 -0700

    first test

--- public/js/calc.js
+++ public/js/calc.js
@@ -1,6 +1,13 @@
+// calculate totals of what I ate
+//
+// eatenFood - [{name: 'egg', qty: 10}, {name: 'chicken', qty: 1}]
+// availableFood - [{name: 'egg', cals: 80, protein: 7, carbs: 3, fat: 6}, {name: 'chicken', cals: 120, protein: 7, carbs: 3, fat: 6}]
+// result - {calories: 100, protein: 50, carbs: 12, fat: 4};
 module.exports.calculateTotal = calculateTotal;
 
-function calculateTotal(food) {
-    return {calories: 100, protein: 50, carbs: 12, fat: 4};
+function calculateTotal(eatenFood, availableFood) {
+  result = {calories: 0, protein: 0, carbs: 0, fat: 0};
+
+  return result;
 };
 
--- test/calc.js
+++ test/calc.js
@@ -1,6 +1,20 @@
+// test using the bulit-in assert module
+
+// == and === are the same except for type conversion made by ==
+// assert.equal uses ==
+// assert.strictEqual uses ===
+// assert.deepEqual doesn't care amount the reference in memory
+
 var assert = require('assert');
 var total = require('../public/js/calc.js');
 
-// assert.equal(total.calculateTotal('test'),{calories: 100, protein: 50, carbs: 12, fat: 4});
-assert.deepEqual(total.calculateTotal('test'), {calories: 100, protein: 50, carbs: 12, fat: 4});
+var eatenFood = [];
+var availableFood = [];
 
+assert.deepEqual(total.calculateTotal(eatenFood, availableFood), {calories: 0, protein: 0, carbs: 0, fat: 0});
+
+
+// eatenFood = [{name: 'egg', qty: 10}];
+// availableFood = [{name: 'egg', cals: 80, protein: 7, carbs: 3, fat: 6}];
+// 
+// assert.deepEqual(total.calculateTotal(eatenFood, availableFood), {calories: 800, protein: 70, carbs: 30, fat: 60});
