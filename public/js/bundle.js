13758ba4273d7a1f339337f6b3e4abe4d362a115
Oren <ogolan@attinteractive.com>
2012-09-08 23:07:54 -0700

    show extra

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -460,61 +460,6 @@ function roundTotal(total) {
 
 });
 
-require.define("/calc.js",function(require,module,exports,__dirname,__filename,process){// calculate totals of what I ate
-//
-// eatenFood - [{name: 'egg', qty: 10}, {name: 'chicken', qty: 1}]
-// availableFood - [{name: 'egg', cal: 80, p: 7, c: 3, f: 6}, {name: 'chicken', cals: 120, p: 7, c: 3, f: 6}]
-// result - {calories: 100, protein: 50, carbs: 12, fat: 4};
-module.exports = calculateTotal;
-
-function calculateTotal(eatenFood, availableFood) {
-  var result = {calories: 0, protein: 0, carbs: 0, fat: 0};
-
-  function calcFoodNutrition(food){
-    var filtered = availableFood.filter(function(element){
-      return (element.name === food.name)
-    });
-
-    result.calories += filtered[0].cal * food.qty;
-    result.protein += filtered[0].p* food.qty;
-    result.carbs += filtered[0].c* food.qty;
-    result.fat += filtered[0].f* food.qty;
-  };
-  
-  eatenFood.forEach(calcFoodNutrition);
-
-  return result;
-};
-
-
-});
-require("/calc.js");
-
-require.define("/calcExtra.js",function(require,module,exports,__dirname,__filename,process){// calculate totals of extra food
-//
-// availableFood - [{name: 'egg', cal: 80, p: 7, c: 3, f: 6}, {name: 'chicken', cals: 120, p: 7, c: 3, f: 6}]
-// result - {"cal":200,"p":27,"c":0,"f":11}
-module.exports = calculateExtraTotal;
-
-function calculateExtraTotal(extraFood) {
-  var result = {cal: 0, p: 0, c: 0, f: 0};
-
-  function addValues(element, index, array) {
-    result.cal += element.cal;
-    result.p += element.p;
-    result.c += element.c;
-    result.f += element.f;
-  };
-
-  extraFood.forEach(addValues);
-
-  return result;
-};
-
-
-});
-require("/calcExtra.js");
-
 require.define("/controllers.js",function(require,module,exports,__dirname,__filename,process){'use strict';
 
 /* Controllers */
@@ -800,4 +745,59 @@ angular.module('calApp').controller('FoodCtrl', function($scope, $http, $cookies
 
 });
 require("/controllers.js");
+
+require.define("/calc.js",function(require,module,exports,__dirname,__filename,process){// calculate totals of what I ate
+//
+// eatenFood - [{name: 'egg', qty: 10}, {name: 'chicken', qty: 1}]
+// availableFood - [{name: 'egg', cal: 80, p: 7, c: 3, f: 6}, {name: 'chicken', cals: 120, p: 7, c: 3, f: 6}]
+// result - {calories: 100, protein: 50, carbs: 12, fat: 4};
+module.exports = calculateTotal;
+
+function calculateTotal(eatenFood, availableFood) {
+  var result = {calories: 0, protein: 0, carbs: 0, fat: 0};
+
+  function calcFoodNutrition(food){
+    var filtered = availableFood.filter(function(element){
+      return (element.name === food.name)
+    });
+
+    result.calories += filtered[0].cal * food.qty;
+    result.protein += filtered[0].p* food.qty;
+    result.carbs += filtered[0].c* food.qty;
+    result.fat += filtered[0].f* food.qty;
+  };
+  
+  eatenFood.forEach(calcFoodNutrition);
+
+  return result;
+};
+
+
+});
+require("/calc.js");
+
+require.define("/calcExtra.js",function(require,module,exports,__dirname,__filename,process){// calculate totals of extra food
+//
+// availableFood - [{name: 'egg', cal: 80, p: 7, c: 3, f: 6}, {name: 'chicken', cals: 120, p: 7, c: 3, f: 6}]
+// result - {"cal":200,"p":27,"c":0,"f":11}
+module.exports = calculateExtraTotal;
+
+function calculateExtraTotal(extraFood) {
+  var result = {cal: 0, p: 0, c: 0, f: 0};
+
+  function addValues(element, index, array) {
+    result.cal += element.cal;
+    result.p += element.p;
+    result.c += element.c;
+    result.f += element.f;
+  };
+
+  extraFood.forEach(addValues);
+
+  return result;
+};
+
+
+});
+require("/calcExtra.js");
 })();
