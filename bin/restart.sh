8f05d9edb6b66b060dcb4f17cd439b2b5bb84ae6
Oren <ogolan@yp.com>
2013-01-03 23:35:35 -0800

    restart

--- bin/restart.sh
+++ bin/restart.sh
@@ -6,7 +6,7 @@ pids="/var/www/$name/shared/pids"
 logs="/var/www/$name/shared/logs"
 
 cd $app
-npm install
+/home/oren/nvm/v0.8.9/bin/npm install
 
 # mon -d "node $app/server.js" -p $pids/$name.pid -l $logs/$name.log --attempts 3 --on-restart $app/bin/email-restart.sh --on-error $app/bin/email-error.sh
 # mon -d "node $app/server.js" -p $pids/$name.pid -l $logs/$name.log
