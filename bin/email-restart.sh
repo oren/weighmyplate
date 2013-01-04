a9c88fb1e1f074ae2858f611d08129d617efa490
Oren <ogolan@yp.com>
2013-01-04 00:26:08 -0800

    full node path

--- bin/email-restart.sh
+++ bin/email-restart.sh
@@ -2,9 +2,9 @@
 
 # echo 'weighmyplate restarted' | mail -s 'weighmyplate restarted' orengolan@gmail.com
 
-command -v node >/dev/null 2>&1 || { echo >&2 "I require node but it's not installed.  Aborting."; exit 1; }
+command -v /home/oren/nvm/v0.8.9/bin/node >/dev/null 2>&1 || { echo >&2 "I require node but it's not installed.  Aborting."; exit 1; }
 
 dir=$(dirname $0)
-cmd="node $dir/email-restart.js"
+cmd="/home/oren/nvm/v0.8.9/bin/node $dir/email-restart.js"
 
 $cmd
