5ef24a98e0d9c1dc60302a1f2cd7d4735c6eac7f
Oren <ogolan@yp.com>
2013-01-04 00:20:48 -0800

    restart mongroup instead of start

--- bin/restart.sh
+++ bin/restart.sh
@@ -8,7 +8,5 @@ logs="/var/www/$name/shared/logs"
 cd $app
 /home/oren/nvm/v0.8.9/bin/npm install
 
-# mon -d "node $app/server.js" -p $pids/$name.pid -l $logs/$name.log --attempts 3 --on-restart $app/bin/email-restart.sh --on-error $app/bin/email-error.sh
-# mon -d "node $app/server.js" -p $pids/$name.pid -l $logs/$name.log
-mongroup start
+mongroup restart
 
